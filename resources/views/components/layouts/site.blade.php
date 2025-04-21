4d52f847d22135914b9b588720d75d69475ec8bd
marvinobig <88102362+marvinobig@users.noreply.github.com>
2025-04-21 19:34:56 +0200

    underline links

--- resources/views/components/layouts/site.blade.php
+++ resources/views/components/layouts/site.blade.php
@@ -10,27 +10,25 @@
     @vite(['resources/js/app.js'])
 </head>
 
-<body>
-    <header>
+<body class="m-4">
+    <header class="mx-auto p-4 max-w-[1000px]">
         <nav>
-            <ul>
-                <li><a href="{{ route('home') }}">Dashboard</a></li>
-                <li><a href="{{ route('posts.create') }}">Create</a></li>
+            <ul class="flex items-center justify-between  w-full capitalize text-xl underline">
+                <li><a href="{{ route('home') }}">dashboard</a></li>
+                <li><a href="{{ route('posts.create') }}">create</a></li>
             </ul>
         </nav>
     </header>
     <main>
-        <section>
-            <x-message />
-        </section>
+        <x-message />
 
-        <section>
+        <section class="p-4 mx-auto max-w-[1000px]">
             {{ $slot }}
         </section>
     </main>
 
-    <footer>
-        <p>&copy; {{ \Carbon\Carbon::now()->year }} Developed by <a href="https://github.com/marvinobig"
+    <footer class="text-center mx-auto p-4 max-w-[1000px]">
+        <p>Copyright &copy; {{ \Carbon\Carbon::now()->year }}. Developed by <a href="https://github.com/marvinobig"
                 target="_blank">marvinobig</a>.</p>
     </footer>
 </body>
