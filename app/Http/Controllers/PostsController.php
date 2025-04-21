284b6447e91b41b5d60b1cf9f267c0059d67d5c1
marvinobig <88102362+marvinobig@users.noreply.github.com>
2025-04-21 14:55:50 +0200

    add controller logic

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StorePostsRequest;
+use App\Http\Requests\UpdatePostsRequest;
+use App\Models\Posts;
+
+class PostsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $posts = Posts::all();
+
+        return view('dashboard', ['posts' => $posts]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('add-posts');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StorePostsRequest $request)
+    {
+        Posts::create($request->validated());
+
+        return redirect('/')->with('message', 'post created');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Posts $posts)
+    {
+        return view('edit-posts', ['post' => $posts]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatePostsRequest $request, Posts $posts)
+    {
+        $posts->update($request->validated());
+
+        return redirect('/')->with('message', 'post updated');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Posts $posts)
+    {
+        $posts->delete();
+
+        return redirect('/')->with('message', 'post deleted');
+    }
+}
