13bced2c9edabe826356caecc1e3af8587603085
marvinobig <88102362+marvinobig@users.noreply.github.com>
2025-04-21 15:05:05 +0200

    correct route declaration

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PostsController::class, 'index']);
 
-Route::resource('/posts', PostsController::class);
+Route::resource('/posts', PostsController::class)->except(['show', 'index']);
