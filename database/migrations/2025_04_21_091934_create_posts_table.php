9ca9cf042a03dff052ec319802a2e6b95f5758c3
marvinobig <88102362+marvinobig@users.noreply.github.com>
2025-04-21 19:48:47 +0200

    make column nullable

--- database/migrations/2025_04_21_091934_create_posts_table.php
+++ database/migrations/2025_04_21_091934_create_posts_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->text('body');
-            $table->boolean('status');
+            $table->boolean('status')->nullable();
             $table->timestamps();
         });
     }
