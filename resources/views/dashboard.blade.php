ffdd7813ed2b8bb61558d9962624df25691ce356
marvinobig <88102362+marvinobig@users.noreply.github.com>
2025-04-21 16:28:16 +0200

    load some post information

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,7 +5,16 @@
 
     <h1>Hello world</h1>
 
-    @foreach ($posts as $post)
-        <p>{{$post->title}}</p>
-    @endforeach
+    <section>
+        @foreach ($posts as $post)
+            <article>
+                <p>{{ $post->title }}</p>
+
+                <div>
+                    <a href="{{ route('posts.edit', $post->id) }}">Edit</a>
+                    <a href="{{ route('posts.destroy', $post->id) }}">Delete</a>
+                </div>
+            </article>
+        @endforeach
+    </section>
 </x-layouts.site>
